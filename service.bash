de8862f1d258d1b7141448c46ea0d68fa6056693
Yu Zhang <yu.zhang.bit@gmail.com>
2016-09-30 23:56:17 -0400

    remove

--- service.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-cd /home/ragdoll/workspace/wiki/wiki-Barebone/
-/home/ragdoll/.rbenv/versions/2.2.4/bin/gollum --config /home/ragdoll/workspace/wiki/wiki-Barebone/config --port 4444
