cb4470553296633ca706cf9cd514622cb68fc95a
yuzhangbit <yu.zhang.bit@gmail.com>
2018-12-03 11:10:06 +0800

    update

--- install.bash
+++ install.bash
@@ -106,7 +106,7 @@ install_rbenv_gollum()
         gem install bundler
     fi
     echo "Installing gollum........"
-		mkdir -p ~/.bundle && sudo chmod -R 777 ~/.bundle && sudo chmod -R 777 ~/.bundle/config
+		mkdir -p ~/.bundle/config && sudo chmod -R 777 ~/.bundle
     bundler install --deployment --gemfile $SCRIPT_DIR/Gemfile
     echo " All ruby packages installed successfully."
 }
